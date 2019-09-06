cb0d185b9b0d4c1e615cc042e65989077f950f94
fixmylifedesigns <ijd.irving@gmail.com>
2019-09-06 12:31:08 -0400

    ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -12,12 +12,32 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
+    results = []
+    hashtable= {}
 
-    return None
+    # if on the length of weights is 1 or none it returns none
+    if len(weights) <= 1:
+        return None
+
+    for x in range(0,len(weights)):
+        # creates a new element that comes from subtracting the limit and weight
+        new = limit - weights[x]
+        # adds that new element to the table
+        hashtable[weights[x]] = new
+
+    for y in range(0, len(weights)):
+        if limit - weights[y] in hashtable:
+            results.insert(0, y)
+    return results
+        
 
 
 def print_answer(answer):
     if answer is not None:
-        print(str(answer[0] + " " + answer[1]))
+        print(str(answer[0] + " " ))
     else:
         print("None")
+
+
+weights_4 = [12, 6, 7, 14, 19, 3, 0, 25, 40]
+print(get_indices_of_item_weights(weights_4, 9, 7))
